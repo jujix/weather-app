2b36bc226a25f4490066ebdf2ec82a1448f97756
jujix <18301246+jujix@users.noreply.github.com>
2021-05-11 13:56:17 +0300

    Add error-handling

--- app.js
+++ app.js
@@ -7,15 +7,21 @@ const url =
   "&query=37.8267,-122.4233&units=m";
 
 request({ url: url, json: true }, (error, response) => {
-  console.log(
-    response.body.current.weather_descriptions[0] +
-      ". It is currently " +
-      response.body.current.temperature +
-      " degrees out. " +
-      "It feels like " +
-      response.body.current.feelslike +
-      " degrees out."
-  );
+  if (error) {
+    console.log("Unable to connect to weather service!");
+  } else if (response.body.error) {
+    console.log("Unable to find a location");
+  } else {
+    console.log(
+      response.body.current.weather_descriptions[0] +
+        ". It is currently " +
+        response.body.current.temperature +
+        " degrees out. " +
+        "It feels like " +
+        response.body.current.feelslike +
+        " degrees out."
+    );
+  }
 });
 
 const geocodeURL =
@@ -23,7 +29,13 @@ const geocodeURL =
   process.env.API_KEY_MAPBOX;
 
 request({ url: geocodeURL, json: true }, (error, response) => {
-  const latitude = response.body.features[0].center[1];
-  const longitude = response.body.features[0].center[0];
-  console.log(latitude, longitude);
+  if (error) {
+    console.log("Unable to connect to location services!");
+  } else if (response.body.features.length === 0) {
+    console.log("Unable to find a location. Try another search");
+  } else {
+    const latitude = response.body.features[0].center[1];
+    const longitude = response.body.features[0].center[0];
+    console.log(latitude, longitude);
+  }
 });
