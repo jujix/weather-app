e1a6f4d669c9f8daf2aa1be2b633c29c50ebcb44
jujix <18301246+jujix@users.noreply.github.com>
2021-05-11 13:11:45 +0300

    Get current weather

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ require("dotenv/config");
 const url =
   "http://api.weatherstack.com/current?access_key=" +
   process.env.API_KEY +
-  "&query=37.8267,-122.4233";
+  "&query=37.8267,-122.4233&units=m";
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request({ url: url, json: true }, (error, response) => {
+  console.log(
+    response.body.current.weather_descriptions[0] +
+      ". It is currently " +
+      response.body.current.temperature +
+      " degrees out. " +
+      "It feels like " +
+      response.body.current.feelslike +
+      " degrees out."
+  );
 });
