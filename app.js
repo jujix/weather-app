4ddb228a644aa823624b59a71584c4235b046157
jujix <18301246+jujix@users.noreply.github.com>
2021-05-12 15:25:51 +0300

    Add express and endpoints for the page

--- app.js
+++ app.js
@@ -1,24 +1,86 @@
 require("dotenv/config");
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-const address = process.argv[2];
+const app = express();
 
-if (!address) {
-  console.log("Please provide an address");
-} else {
-  geocode(address, (error, { latitude, longitude, location } = {}) => {
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Jujix",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Jujix",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "This is some helpfull text",
+    title: "Help",
+    name: "Jujix",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide a address!",
+    });
+  }
+
+  geocode(req.query.address, (error, { latitude, longitude, location }) => {
     if (error) {
-      return console.log(error);
+      return res.send({ error });
     }
 
     forecast(latitude, longitude, (error, forecastData) => {
       if (error) {
-        return console.log(error);
+        return res.send({ error });
       }
 
-      console.log(location);
-      console.log(forecastData);
+      res.send({
+        forecast: forecastData,
+        location,
+        address: req.query.address,
+      });
     });
   });
-}
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Jujix",
+    errorMessage: "Help article not found",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Jujix",
+    errorMessage: "Page not found",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is up on port 3000.");
+});
